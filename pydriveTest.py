b16fda34a692257e04fa423bf9a074d08c3ad217
FranSuarez99 <fransua@javerianacali.edu.co>
2022-04-13 17:22:55 -0500

    a = 1

--- pydriveTest.py
+++ pydriveTest.py
@@ -6,6 +6,8 @@ tugfa.LocalWebserverAuth()
 
 drive = GoogleDrive(tugfa)
 
+a = 1
+
 file1 = drive.CreateFile({'title': 'HelloP.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
 file1.SetContentString('Hello World! P de Prueba') # Set content of the file from given string.
 file1.Upload()
