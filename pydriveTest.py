1c02fe51238526dce4e9c220869f0f307c94c471
FranSuarez99 <fransua@javerianacali.edu.co>
2022-04-13 16:59:31 -0500

    basic files

---
+++ pydriveTest.py
@@ -0,0 +1,11 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+tugfa = GoogleAuth()
+tugfa.LocalWebserverAuth()
+
+drive = GoogleDrive(tugfa)
+
+file1 = drive.CreateFile({'title': 'HelloP.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
+file1.SetContentString('Hello World! P de Prueba') # Set content of the file from given string.
+file1.Upload()
