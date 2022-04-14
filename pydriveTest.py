074f895f2cd5996c568014d17ec00ffe764dd8ea
FranSuarez99 <fransua@javerianacali.edu.co>
2022-04-13 19:44:11 -0500

    file_list

--- pydriveTest.py
+++ pydriveTest.py
@@ -8,8 +8,14 @@ drive = GoogleDrive(tugfa)
 
 a = 1
 
-file1 = drive.CreateFile({'title': 'HelloP.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
-file1.SetContentString('Hello World! P de Prueba') # Set content of the file from given string.
-file1.Upload()
+#file1 = drive.CreateFile({'title': 'HelloP.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
+#file1.SetContentString('Hello World! P de Prueba') # Set content of the file from given string.
+#file1.Upload()
+
+file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+for file1 in file_list:
+  print('title: %s, id: %s' % (file1['title'], file1['id']))
+  if file1['title'] == 'palabras':
+      palabrasID = file1['id']
 
 #prueba commit suribe
