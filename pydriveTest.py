74070d34ed73e03dcccdfe7846cc50d4c39f1af7
FranSuarez99 <fransua@javerianacali.edu.co>
2022-04-13 19:47:33 -0500

    getContent

--- pydriveTest.py
+++ pydriveTest.py
@@ -6,16 +6,18 @@ tugfa.LocalWebserverAuth()
 
 drive = GoogleDrive(tugfa)
 
-a = 1
-
 #file1 = drive.CreateFile({'title': 'HelloP.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
 #file1.SetContentString('Hello World! P de Prueba') # Set content of the file from given string.
 #file1.Upload()
+
+palabrasID = 0
 
 file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
 for file1 in file_list:
   print('title: %s, id: %s' % (file1['title'], file1['id']))
   if file1['title'] == 'palabras':
       palabrasID = file1['id']
+palabras = drive.CreateFile({'id': palabrasID})
+palabras.GetContentFile('palabras.txt')
 
 #prueba commit suribe
