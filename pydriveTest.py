bf1901e9b10fa779cbb9d0d028f11a178f488ca6
FranSuarez99 <fransua@javerianacali.edu.co>
2022-04-26 16:12:19 -0500

    4 files

--- pydriveTest.py
+++ pydriveTest.py
@@ -10,16 +10,27 @@ drive = GoogleDrive(tugfa)
 #file1.SetContentString('Hello World! P de Prueba') # Set content of the file from given string.
 #file1.Upload()
 
-palabrasID = 0
+fileName1 = 'words.txt'
+fileName2 = 'solutions.txt'
+fileName3 = 'difficulty.txt'
+fileName4 = 'imgSource.txt'
+
+wordsID, solutionsID, difficultyID, imgSource = 0, 0, 0, 0
 
 file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
 for file1 in file_list:
   print('title: %s, id: %s' % (file1['title'], file1['id']))
-  if file1['title'] == 'palabras.txt':
-      palabrasID = file1['id']
-palabras = drive.CreateFile({'id': palabrasID})
-palabras.GetContentFile('palabras.txt')
-content = palabras.GetContentString().strip().split()
+  if file1['title'] == fileName1:
+      wordsID = file1['id']
+  if file1['title'] == fileName2:
+      solutionsID = file1['id']
+  if file1['title'] == fileName3:
+      difficultyID = file1['id']
+  if file1['title'] == fileName4:
+      imgSource = file1['id']
+words = drive.CreateFile({'id': wordsID})
+words.GetContentFile('palabras.txt')
+content = words.GetContentString().strip().split()
 
 print(content)
 
