6041f3a663ab90504f825be55651a4892adbeb4e
suribe06 <santiu990506@gmail.com>
2022-05-01 18:19:35 -0500

    push

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
-import requests, csv, sys, os
+import requests, sys
 from flask import *
 
 from pyDriveFunct import *
@@ -91,10 +91,7 @@ def game_view():
         wordsList = getWords(difficult, difficulty)
         word = wordsList.pop()
     session['wordsList'] = wordsList
-    file = open("salidaW.txt", "a")
-    file.write(str(word)+"\n")
-    file.close()
-    photo_source = None
+    photo_source = f'{word}.png'
     if request.method == 'POST':
         if request.form["btn"] == "¡Enviar!":
             num_sounds = int(request.form.get("letterNum"))
@@ -104,9 +101,6 @@ def game_view():
                 sound = int(request.form.get(name_box))
                 child_solution.append(sound)
             ans = (child_solution == solutions[word])
-            file = open("salida.txt", "a")
-            file.write(str(ans)+"\n")
-            file.close()
     return render_template('game.html', photo_source=photo_source)
 
 if __name__ == "__main__":
