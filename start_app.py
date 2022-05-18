f4b090844fa4fd9198d1c9d6f503722b8cf6eb60
FranSuarez99 <fransua@javerianacali.edu.co>
2022-05-17 21:58:04 -0500

    open webpage

--- start_app.py
+++ start_app.py
@@ -8,9 +8,9 @@ if oper_sys == 'Linux':
     os.system('export FLASK_ENV=development')
     os.system('flask run')
 elif oper_sys == 'Windows':
+    os.system('start http://localhost:5000/')
     os.system('set FLASK_APP=app.py')
     os.system('set FLASK_ENV=development')
     os.system('flask run')
-    os.system('start http://localhost:5000/')
 else:
     pass
